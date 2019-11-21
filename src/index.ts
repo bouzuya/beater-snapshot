316832b482cc025fe89ae5f7a3033984c2782500
bouzuya <m@bouzuya.net>
2019-11-21 21:46:15 +0900

    add stringify to options

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ type SnapshotOptions = {
   directory: string;
   load: (p: string) => string;
   save: (p: string, data: string) => void;
+  stringify: (o: any) => string;
   update: boolean;
 };
 
@@ -19,13 +20,13 @@ const formatKey =
   (name: string): SnapshotKey =>
     name.replace(/[^-_a-zA-Z0-9\/]/g, '_') + '.json';
 
-const formatValue =
+const defaultStringify =
   (o: any): SnapshotValue => {
     if (o === void 0) throw new Error('actual is not supported value');
     return JSON.stringify(o, null, 2);
   };
 
-const parseValue =
+const defaultParse =
   (value: SnapshotValue | null) =>
     value === null ? null : JSON.parse(value);
 
@@ -46,23 +47,31 @@ const ensureOptions = (options?: Partial<SnapshotOptions>): SnapshotOptions => {
   const directory = options?.directory ?? '__snapshots__';
   const load = options?.load ?? defaultLoad;
   const save = options?.save ?? defaultSave;
+  const stringify = options?.stringify ?? defaultStringify;
   const update = options?.update ?? process.env.UPDATE_SNAPSHOT === 'true';
   return {
     directory,
     load,
     save,
+    stringify,
     update
   };
 };
 
 const init = (options?: Partial<SnapshotOptions>): Snapshot => {
-  const { directory, load, save, update } = ensureOptions(options);
+  const {
+    directory,
+    load,
+    save,
+    stringify,
+    update
+  } = ensureOptions(options);
   return async <T>(name: string, o: T): Promise<T> => {
     const p = path.join(directory, formatKey(name));
-    const actual = formatValue(o);
+    const actual = stringify(o);
     if (update) save(p, actual);
     const expected = load(p);
-    return parseValue(expected);
+    return defaultParse(expected);
   };
 };
 
